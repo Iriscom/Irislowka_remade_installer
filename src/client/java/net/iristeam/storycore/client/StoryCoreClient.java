e50694ed8cf1683eeb289464ef2618a601edffe8
PsychoicCoder <158079502+PsychoicCoder@users.noreply.github.com>
2024-12-23 14:24:43 +0300

    first commit

---
+++ src/client/java/net/iristeam/storycore/client/StoryCoreClient.java
@@ -0,0 +1,10 @@
+package net.iristeam.storycore.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class StoryCoreClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/net/iristeam/storycore/StoryCore.java
@@ -0,0 +1,10 @@
+package net.iristeam.storycore;
+
+import net.fabricmc.api.ModInitializer;
+
+public class StoryCore implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
