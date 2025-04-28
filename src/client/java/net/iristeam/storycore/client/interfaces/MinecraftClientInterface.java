c88d6cc5394eef738e65a2ebb828e47957c9c4b4
Iriscom <145111472+Iriscom@users.noreply.github.com>
2025-04-28 14:24:30 +0300

    Delete src directory

--- src/client/java/net/iristeam/storycore/client/interfaces/MinecraftClientInterface.java
+++
@@ -1,7 +0,0 @@
-package net.iristeam.storycore.client.interfaces;
-
-public interface MinecraftClientInterface {
-    public long getStartTime();
-    public boolean isTeleporting();
-    public void setTeleporting(boolean Telep);
-}
--- src/main/java/net/iristeam/storycore/init/InitMob.java
+++
@@ -1,8 +0,0 @@
-package net.iristeam.storycore.init;
-import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
-
-public class InitMob {
-    void onInit(){
-//        FabricDefaultAttributeRegistry.register(ModEntities.NPC, NPCEntity.createNPCAttributes());
-    }
-}
